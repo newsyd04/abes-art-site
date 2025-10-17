20a928ad127f0d833772505cb5d9d53826d9cc4f
newsyd04 <newsyd04@gmail.com>
2025-10-17 14:07:13 +0100

    removed github pages stuff

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,9 @@ import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
 
-const isProduction = import.meta.env.MODE === "production";
-const basename = isProduction ? "/abes-art-site" : "/";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
